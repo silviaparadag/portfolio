bdf3e72ccc79397285ea6dc5d076e5348b4a83d8
Silvia Parada <silviaparadag@yahoo.es>
2023-11-03 16:58:41 +0100

    Start html header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,29 @@
-import logo from '../images/faviconSP.png';
+import logo from '../images/sp_logo-white.svg';
 import '../styles/layout/Header.scss';
 
 const Header = () => {
   return (
     <header className="header">
-      <a to="/">
-        <img src={logo} alt="Rick and Morty" className="header__img" />
-      </a>
+      <div className="container">
+        <a to="/">
+          <img src={logo} alt="Rick and Morty" className="header__logo" />
+        </a>
+        <nav className="nav">
+          <a href="#about-me" className="nav__link">
+            About me
+          </a>
+          <a href="#projects" className="nav__link">
+            Projects
+          </a>
+          <a href="#resume" className="nav__link">
+            CV | Resume
+          </a>
+          <a href="#contact" className="nav__link--button">
+            Contact
+          </a>
+          {/* */}
+        </nav>
+      </div>
     </header>
   );
 };
