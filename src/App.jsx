131c4b0fcc679782db108f733b3137d3b4886b6f
Silvia Parada <silviaparadag@yahoo.es>
2023-11-07 19:24:25 +0100

    Create components and routes structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Hero from './components/Hero';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
+import HomeHero from './components/HomeHero';
+import HomeProjects from './components/HomeProjects';
+import HomeContact from './components/HomeContact';
+import HomeResume from './components/HomeResume';
 import Landing from './components/Landing';
+import AboutMe from './components/About';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
 //import NotFoundPage from './NotFoundPage';
 import './styles/App.scss';
 
@@ -22,14 +25,17 @@ function App() {
             <div className="App">
               <Header />
               <main className="main">
-                <Hero />
-                <Projects />
-                <Resume />
-                <Contact />
+                <HomeHero />
+                <HomeProjects />
+                <HomeResume />
+                <HomeContact />
               </main>
             </div>
           }
         />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
         {/* <Route
           path="/project/:id"
           element={
