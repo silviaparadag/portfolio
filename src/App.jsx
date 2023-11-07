d2052b782de7447cd0cc3fec8188a63c2719e40a
Silvia Parada <silviaparadag@yahoo.es>
2023-11-07 19:05:25 +0100

    Create JSX componentes and text for section in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 import Landing from './components/Landing';
 //import NotFoundPage from './NotFoundPage';
 import './styles/App.scss';
-import route from './images/route-white.png';
 
 function App() {
   return (
@@ -19,102 +22,10 @@ function App() {
             <div className="App">
               <Header />
               <main className="main">
-                <section className="hero">
-                  <div className="hero__container">
-                    <h1 className="hero__container--title">
-                      Introducing Silvia
-                    </h1>
-                    <div className="hero__container--subtitle">
-                      Jr. Frontend Developer & Architect{' '}
-                    </div>
-                    <p className="hero__container--text">
-                      Ipsum praesentium similique ratione laborum nostrum
-                      laudantium, assumenda optio consequatur neque provident
-                      dolore libero atque, molestias exercitationem tempora
-                      consequuntur repellat magnam nisi!
-                    </p>
-                  </div>
-                </section>
-                <section className="section">
-                  <div className="section__container">
-                    <div className="section__container--projects">
-                      <div className="section__container--project"></div>
-                      <div className="section__container--project"></div>
-                      <div className="section__container--project"></div>
-                    </div>
-
-                    <h2 className="section__container--title">Projects</h2>
-                    <p className="section__container--text">
-                      Ipsum praesentium similique ratione laborum nostrum
-                      laudantium, assumenda optio consequatur neque provident
-                      dolore libero atque, molestias exercitationem tempora
-                      consequuntur repellat magnam nisi!
-                    </p>
-                  </div>
-                </section>
-                <section className="section">
-                  <div className="section__container">
-                    <img
-                      src={route}
-                      alt=""
-                      className="section__container--img"
-                    />
-
-                    <h2 className="section__container--title">
-                      My profesional journey
-                    </h2>
-                    <p className="section__container--text">
-                      Ipsum praesentium similique ratione laborum nostrum
-                      laudantium, assumenda optio consequatur neque provident
-                      dolore libero atque, molestias exercitationem tempora
-                      consequuntur repellat magnam nisi!
-                    </p>
-                  </div>
-                </section>
-                <section className="section">
-                  <div className="section__container">
-                    <h2 className="section__container--title">Contact me</h2>
-                    <form className="form" action="">
-                      <label htmlFor="name" className="form__label">
-                        Name
-                      </label>
-                      <input
-                        type="text"
-                        name="name"
-                        id="name"
-                        placeholder="Paquita Salas"
-                        className="form__field"
-                        required
-                      />
-                      <label htmlFor="email" className="form__label">
-                        Email
-                      </label>
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        placeholder="paquita@psmanagemenet.com"
-                        className="form__field"
-                        required
-                      />
-                      <label htmlFor="message" className="form__label">
-                        Message
-                      </label>
-                      <textarea
-                        id="message"
-                        name="message"
-                        placeholder="Your message"
-                        className="form__textarea"
-                        required
-                      />
-                      <input
-                        type="submit"
-                        value="Submit"
-                        className="form__button"
-                      />
-                    </form>
-                  </div>
-                </section>
+                <Hero />
+                <Projects />
+                <Resume />
+                <Contact />
               </main>
             </div>
           }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import '../styles/layout/Main.scss';
+
+const Hero = () => {
+  return (
+    <>
+      <section className="hero">
+        <div className="hero__container">
+          <h1 className="hero__container--title">Introducing Silvia</h1>
+          <div className="hero__container--subtitle">
+            Jr. Frontend Developer & Architect{' '}
+          </div>
+          <p className="hero__container--text">
+            Looking for new challenges in IT sector as Devoloper or Product
+            Manager where I bring my experience of 15+ years in management and
+            design. <br />
+            Enthusiasm and passion for everything I work on.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
