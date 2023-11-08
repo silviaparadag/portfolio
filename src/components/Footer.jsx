9a9eb29473d0695881bf3035da2fe27caf600221
Silvia Parada <silviaparadag@yahoo.es>
2023-11-08 20:50:55 +0100

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import '../styles/layout/Footer.scss';
+import logo from '../images/s-p_logo.svg';
 
 const Footer = () => {
   return (
-    <footer className="footer ">
-      <span className="footer__text">
-        ©
+    <footer className="footer">
+      <div className="footer__grid">
+        <a to="/">
+          <img src={logo} alt="Silvia Logo" className="header__logo" />
+        </a>
+        <div></div>
+      </div>
+      <div className="footer__footer">
+        {/* <p>©</p>
         <a
           href="https://www.linkedin.com/in/silviaparadag/"
           target="_blank"
@@ -13,9 +20,9 @@ const Footer = () => {
         >
           {' '}
           Silvia Parada |
-        </a>
-      </span>
-      <span className="footer__text">| 2023</span>
+        </a> */}
+        <p className="footer__text">© Silvia Parada | 2023</p>
+      </div>
     </footer>
   );
 };
