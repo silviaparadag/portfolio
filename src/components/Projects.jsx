ba60025d75e96fad7c3b6f8cc138ac4c26f972f2
Silvia Parada <silviaparadag@yahoo.es>
2023-11-09 21:18:09 +0100

    Modify card project structure, add icons/link to project repo and url. Some styles in cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,9 +12,36 @@ const Projects = (props) => {
             className="projectCard__img"
           />
           <div className="projectCard__info">
-            <h2 className="projectCard__title">{eachProject.title}</h2>
-            <p className="projectCard__text">{eachProject.desc}</p>
-            <p className="projectCard__text">{eachProject.type}</p>
+            <h2 className="projectCard__info--title">{eachProject.title}</h2>
+            <p className="projectCard__info--text1">{eachProject.desc}</p>
+            <p className="projectCard__info--text2">{eachProject.type}</p>
+          </div>
+          <div className="projectCard__icons">
+            <a
+              href={eachProject.repo}
+              className="projectCard__icons--link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-github-alt"></i>
+            </a>
+            <a
+              href={eachProject.url}
+              className="projectCard__icons--link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-solid fa-globe"></i>
+            </a>
+
+            <a
+              href={eachProject.repo}
+              className="projectCard__icons--link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-solid fa-gear"></i>
+            </a>
           </div>
         </article>
       </li>
