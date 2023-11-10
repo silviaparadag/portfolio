469d98dedd308d0d074956623f3dcb95387e3c17
Silvia Parada <silviaparadag@yahoo.es>
2023-11-10 21:54:27 +0100

    Prepare to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
-  base: '/portfolio/',
+  base: 'https://silviaparadag.github.io/portfolio/',
 });
 
 //base: 'https://silviaparadag.github.io/portfolio/',
