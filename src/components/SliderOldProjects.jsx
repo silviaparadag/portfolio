f36a082f1ed4cc7a8a68771a97ad0b775bc8107a
Silvia Parada <silviaparadag@yahoo.es>
2023-12-01 12:11:41 +0100

    Change carousel library

--- src/components/SliderOldProjects.jsx
+++ src/components/SliderOldProjects.jsx
@@ -1,37 +1,55 @@
-import Slider from 'react-slick';
+import ImageGallery from 'react-image-gallery';
 import PropTypes from 'prop-types';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import 'react-image-gallery/styles/scss/image-gallery.scss';
 
 const SliderOldProjects = (props) => {
   const pictures = props.jobsList.flatMap((eachJob) =>
-    eachJob.pictures.map((eachImg, index) => (
-      <div key={index} className="sectionResume3__container2">
-        <img
-          src={eachImg.img}
-          alt={`Picture of ${eachImg.name}`}
-          className="sectionResume3__container2--img"
-        />
-      </div>
-    ))
+    eachJob.pictures.map((eachImg) => ({
+      original: eachImg.img,
+      // description: eachImg.name,
+      renderItem: (item) => (
+        <div className="image-gallery-image">
+          <img
+            src={item.original}
+            alt={item.description}
+            style={{ width: '300px' }}
+          />
+          {item.description && (
+            <span className="image-gallery-description">
+              {item.description}
+            </span>
+          )}
+        </div>
+      ),
+      renderLeftNav: (onClick, disabled) => (
+        <button
+          className={`image-gallery-left-nav ${disabled ? 'disabled' : ''}`}
+          onClick={onClick}
+        >
+          {/* Personaliza la apariencia de la flecha izquierda aquí */}
+          &lt;
+        </button>
+      ),
+      renderRightNav: (onClick, disabled) => (
+        <button
+          className={`image-gallery-right-nav ${disabled ? 'disabled' : ''}`}
+          onClick={onClick}
+        >
+          {/* Personaliza la apariencia de la flecha derecha aquí */}
+          &gt;
+        </button>
+      ),
+    }))
   );
-  const settings = {
-    // arrows: false,
-    dots: true,
-    centerMode: true,
-    // autoplay: true,
-    // autoplaySpeed: 3000,
-    // fade: true,
-    // adaptiveHeight: true,
-    centerPadding: '40px',
-    pauseOnHover: true,
-    infinite: true,
-    speed: 100,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+
+  const options = {
+    showFullscreenButton: false,
+    showPlayButton: false,
+    showNav: true,
+    showThumbnails: false,
   };
 
-  return <Slider {...settings}>{pictures}</Slider>;
+  return <ImageGallery {...options} items={pictures} />;
 };
 
 export default SliderOldProjects;
