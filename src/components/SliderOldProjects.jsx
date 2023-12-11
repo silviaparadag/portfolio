d5e57c8e82ab9f7d9a4ed6391330b75a1c5c365a
Silvia Parada <silviaparadag@yahoo.es>
2023-12-11 20:02:40 +0100

    Start education section

--- src/components/SliderOldProjects.jsx
+++ src/components/SliderOldProjects.jsx
@@ -5,45 +5,45 @@ import 'react-image-gallery/styles/scss/image-gallery.scss';
 
 const SliderOldProjects = (props) => {
   const pictures = props.jobsList.flatMap((eachJob) =>
-    eachJob.pictures.map((eachImg) => ({
-      original: eachImg.img,
-      thumbnail: eachImg.img,
-      description: (
-        <p className="sectionResume3__container2--text">{eachImg.name}</p>
-      ),
-      // originalTitle: (
-      //   <p className="sectionResume3__container2--text">{eachImg.name}</p>
-      // ),
-      renderItem: (item) => (
-        <div className="image-gallery-image">
-          <img
-            className="sectionResume3__container2--img"
-            src={item.original}
-            alt={item.description}
-            // style={{ width: '450px' }}
-          />
-          {item.description && (
-            <p className="image-gallery-description">{item.description}</p>
-          )}
-        </div>
-      ),
-      renderLeftNav: (onClick, disabled) => (
-        <button
-          className={`image-gallery-left-nav ${disabled ? 'disabled' : ''}`}
-          onClick={onClick}
-        >
-          &lt;
-        </button>
-      ),
-      renderRightNav: (onClick, disabled) => (
-        <button
-          className={`image-gallery-right-nav ${disabled ? 'disabled' : ''}`}
-          onClick={onClick}
-        >
-          &gt;
-        </button>
-      ),
-    }))
+    eachJob.pictures
+      .filter((eachImg) => eachImg.img)
+      .map((eachImg) => ({
+        original: eachImg.img,
+        thumbnail: eachImg.img,
+        description: (
+          <p className="sectionResume3__container2--text">{eachImg.name}</p>
+        ),
+
+        renderItem: (item) => (
+          <div className="image-gallery-image">
+            <img
+              className="sectionResume3__container2--img"
+              src={item.original}
+              alt={item.description}
+              // style={{ width: '450px' }}
+            />
+            {item.description && (
+              <p className="image-gallery-description">{item.description}</p>
+            )}
+          </div>
+        ),
+        renderLeftNav: (onClick, disabled) => (
+          <button
+            className={`image-gallery-left-nav ${disabled ? 'disabled' : ''}`}
+            onClick={onClick}
+          >
+            &lt;
+          </button>
+        ),
+        renderRightNav: (onClick, disabled) => (
+          <button
+            className={`image-gallery-right-nav ${disabled ? 'disabled' : ''}`}
+            onClick={onClick}
+          >
+            &gt;
+          </button>
+        ),
+      }))
   );
 
   const options = {
@@ -51,7 +51,6 @@ const SliderOldProjects = (props) => {
     showPlayButton: false,
     showNav: true,
     showThumbnails: true,
-    // thumbnailPosition: 'left',
   };
 
   return <ImageGallery {...options} items={pictures} />;
@@ -62,3 +61,8 @@ export default SliderOldProjects;
 SliderOldProjects.propTypes = {
   jobsList: PropTypes.array,
 };
+
+// originalTitle: (
+//   <p className="sectionResume3__container2--text">{eachImg.name}</p>
+// ),
+// thumbnailPosition: 'left',
