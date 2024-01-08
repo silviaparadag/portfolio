ef7cdbd985e3679e80baf73187fd32c4c179849c
Silvia Parada <silviaparadag@yahoo.es>
2024-01-08 12:19:33 +0100

    Start responsive design: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,16 @@ import '../styles/layout/Header.scss';
 //import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import logo from '../images/s-p_logo.svg';
+import { useState } from 'react';
 
 const Header = (props) => {
+  const [isNavShown, setIsNavShown] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavShown(!isNavShown);
+    console.log();
+  };
+
   const contactButton = props.isHomePage ? (
     <button onClick={props.scrollToContact} className="nav__link--button">
       Contact
@@ -14,13 +22,28 @@ const Header = (props) => {
     </a>
   );
 
+  const navigationMenu = () => (
+    <nav className={`nav ${isNavShown ? 'nav--shown' : ''}`}>
+      <a href="#aboutme" className="nav__link">
+        About me
+      </a>
+      <a href="#projects" className="nav__link">
+        Projects
+      </a>
+      <a href="#resume" className="nav__link">
+        CV
+      </a>
+      {contactButton}
+    </nav>
+  );
+
   return (
     <header className="header">
       <>
         <a href="/portfolio/">
           <img src={logo} alt="Silvia Parada Logo" className="header__logo" />
         </a>
-        <nav className="nav">
+        {/* <nav className="nav">
           <a href="#aboutme" className="nav__link">
             About me
           </a>
@@ -31,7 +54,13 @@ const Header = (props) => {
             CV
           </a>
           {contactButton}
-        </nav>
+        </nav> */}
+        <div className="menu">
+          <div className="menu__icon" onClick={toggleNav}>
+            ☰
+          </div>
+        </div>
+        {navigationMenu()}
       </>
     </header>
   );
