6c56d2e1f89b2fc55aea7e3f3d3a3840c20efad1
Silvia Parada <silviaparadag@yahoo.es>
2024-01-09 13:35:55 +0100

    Responsive design: projects page and about

--- src/components/ProjectsHero.jsx
+++ src/components/ProjectsHero.jsx
@@ -8,8 +8,13 @@ const ProjectsHero = (props) => {
       <div className="heroProjects__container">
         <h2 className="heroProjects__container--title">Projects</h2>
         <p className="heroProjects__container--text">
-          Explore a portfolio of my technological works, where you will discover
-          the main projects I have done during the last year.
+          Welcome to my Projects page, explore a portfolio of my latest tech
+          work, where you'll discover the main projects I've done during the
+          last year, mainly during my bootcamp at Adalab. Whether you are a
+          fellow developer, a potential client, or just a curious soul or
+          recruiter, I invite you to browse through them, in fact, you can
+          discover more in each GitHub repository! I hope you find something
+          that interests you.
         </p>
       </div>
       <FilterForm
