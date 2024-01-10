b67267bd07884a936b3f6b911c7dd5f9779c5790
Silvia Parada <silviaparadag@yahoo.es>
2024-01-10 11:36:39 +0100

    Responsive design: start with resume page

--- src/components/ProjectsHero.jsx
+++ src/components/ProjectsHero.jsx
@@ -18,11 +18,11 @@ const ProjectsHero = (props) => {
         <p className="heroProjects__container--text">
           Welcome to my Projects page, explore a portfolio of my latest tech
           work, where you'll discover the main projects I've done during the
-          last year, mainly during my bootcamp at Adalab. Whether you are a
-          fellow developer, a potential client, or just a curious soul or
-          recruiter, I invite you to browse through them, in fact, you can
-          discover more in each GitHub repository! I hope you find something
-          that interests you.
+          last year, mainly during my bootcamp at Adalab. <br />
+          Whether you are a fellow developer, a potential client, or just a
+          curious soul or recruiter, I invite you to browse through them, and
+          you can even discover more in each GitHub repository! <br />I hope you
+          find something that interests you.
         </p>
       </div>
       <FilterForm
